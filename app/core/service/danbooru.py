77145b3a70339392391e27ac5f3dcef68de4820a
majaja <majajashka@hotmail.com>
2024-06-10 04:23:53 +0400

    refactor(user_args_parse) separate business logic

--- app/core/service/danbooru.py
+++ app/core/service/danbooru.py
@@ -1,7 +1,27 @@
 from typing import Collection, Optional
 
-from app.core.models.dto.api.danbooru import DanbooruPost
+from app.core.models.dto.api.danbooru import DanbooruPost, DanbooruRequestArgs
 from app.core.external_service.api.danbooru import DanbooruAPI
+from app.core.models.dto.api.parse_config import ParseConfig
+from app.core.utils.expections import UserArgumentError
+from app.core.utils.parse_args import parse_args_for_post
+
+
+class DanbooruRequest:
+    pass
+
+
+async def parse_user_danbooru_args(user_args: str, parse_config: ParseConfig) -> DanbooruRequestArgs:
+    try:
+        parsed_args = parse_args_for_post(user_args=user_args)
+    except ValueError as e:
+        raise UserArgumentError(f'Invalid arguments: {e}', user_args=user_args) from e
+    if parsed_args.count > parse_config.max_count:
+        raise UserArgumentError(
+            message="The number of requests shouldn't exceed 10",
+            user_args=user_args
+        )
+    return parsed_args
 
 
 async def get_danbooru_post(tags: Optional[Collection[str]] = None) -> DanbooruPost:
--- app/core/utils/parse_args.py
+++ app/core/utils/parse_args.py
@@ -4,23 +4,21 @@ from app.core.models.dto.api.danbooru import DanbooruRequestArgs
 
 
 def parse_args_for_post(
-        args: Optional[str],
+        user_args: Optional[str],
         default_count: int = 1,
         default_tags: Optional[Collection[str]] = None
 ) -> DanbooruRequestArgs:
-    if not args:
+    if not user_args:
         return DanbooruRequestArgs(count=default_count, tags=default_tags)
     if not default_tags:
         default_tags = []
 
-    arguments = args.split()
+    arguments = user_args.split()
     if arguments[0].isdigit():
         count = int(arguments[0])
-        if count > 10:
-            raise ValueError('The number of requests should not exceed 10')
         tags = arguments[1:]
     else:
         count = default_count
         tags = arguments
 
-    return DanbooruRequestArgs(count=count, tags=set(tags+default_tags))
+    return DanbooruRequestArgs(count=count, tags=set(tags + default_tags))
