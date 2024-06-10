da9c879808a32174ecff105165ad0a7b934bdf83
majaja <majajashka@hotmail.com>
2024-06-10 04:25:10 +0400

    feat(BaseExternalAPI) add specifying the type of error

--- app/core/external_service/api/base.py
+++ app/core/external_service/api/base.py
@@ -1,12 +1,17 @@
+from typing import Type
 from aiohttp import ClientSession, ClientResponse
 
-from app.core.external_service.api.expections import ApiError
+from app.core.utils.expections import ApiError
 
 
 class BaseExternalAPI:
-    def __init__(self, url: str):
-        self.session = ClientSession()
+    def __init__(self, url: str, error: Type[ApiError] = ApiError):
         self.base_url = url
+        self.session = ClientSession()
+
+        if not issubclass(error, ApiError):
+            raise ValueError(f'{error} is not subclass of ApiError')
+        self.error = error
 
     async def _handle_error(self, response: ClientResponse, description: str = None) -> None:
         """
@@ -14,6 +19,8 @@ class BaseExternalAPI:
 
          :param response: The HTTP response object.
          :type response: aiohttp.ClientResponse
+         :param description: Error description from response
+         :type description: str
 
          :raises ApiError: If an error occurs during the API request.
         """
@@ -21,7 +28,7 @@ class BaseExternalAPI:
             description = 'Unknown error'
 
         if response.status >= 400:
-            raise ApiError(
+            raise self.error(
                 api_name=self.__class__.__name__,
                 response_status=response.status,
                 url=response.url.__str__(),
@@ -32,7 +39,7 @@ class BaseExternalAPI:
         url = f"{self.base_url}/{endpoint}"
         async with self.session.request(method, url, params=params, data=data) as response:
             data = await response.json()
-            await self._handle_error(response, data)
+            await self._handle_error(response)
             return data
 
     async def get(self, endpoint: str, params=None):
