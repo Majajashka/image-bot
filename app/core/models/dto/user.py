b83552be09a38087477ce9438a4bbcf6b0971785
majaja <majajashka@hotmail.com>
2024-06-15 01:10:47 +0400

    feat(UserDTO): add __str__ and __repr__ method

--- app/core/models/dto/user.py
+++ app/core/models/dto/user.py
@@ -11,6 +11,13 @@ class User(Base):
     created_at: datetime = None
     updated_at: datetime = None
 
+    def __str__(self):
+        return f'ID: {self.id}, Active: {self.active}, Language: {self.language}'
+
+    def __repr__(self):
+        return (f"User(id={self.id}, active={self.active}, language={self.language}, "
+                f"created_at={self.created_at}, updated_at={self.updated_at})")
+
 
 @dataclass
 class UserActivity(Base):
