305f3b57bcf101051b91bc274ff1f12f6cb15d47
majaja <majajashka@hotmail.com>
2024-06-17 16:01:13 +0400

    feat(DanbooruConstants): add limit for posts at one time

--- app/core/constants/danbooru.py
+++ app/core/constants/danbooru.py
@@ -28,3 +28,7 @@ FREE_TAGS = [
 
 BASIC_TAGS = ['is:sfw']  # Tags that will be applied to every request
 USER_DEFAULT_TAGS = ['score:>25']  # Default tags for new users
+
+# Limit for posts at one time
+MAX_USER_POST_COUNT = 10
+MAX_ADMIN_POST_COUNT = 100
