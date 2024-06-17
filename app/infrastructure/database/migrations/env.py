7b28bd894b750d2b34b22050c85477956e11f188
majaja <majajashka@hotmail.com>
2024-06-17 14:31:14 +0400

    add models

--- app/infrastructure/database/migrations/env.py
+++ app/infrastructure/database/migrations/env.py
@@ -12,6 +12,9 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 
 from app.infrastructure.database.models.base import Base
+from app.infrastructure.database.models.user import UserOrm
+from app.infrastructure.database.models.danbooru import DanbooruOrm
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
