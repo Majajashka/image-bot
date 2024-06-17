c8b3cbca50a437ff9412682456d4e586026ea36c
majaja <majajashka@hotmail.com>
2024-06-17 15:00:22 +0400

    feat(UserRepo): change id to tg_id add banned status and relationship

--- app/infrastructure/database/models/user.py
+++ app/infrastructure/database/models/user.py
@@ -1,5 +1,5 @@
-from sqlalchemy import BigInteger, true
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import BigInteger, true, false
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.core.models.dto.user import User
 from app.infrastructure.database.models.base import Base, TimestampMixin
@@ -8,20 +8,24 @@ from app.infrastructure.database.models.base import Base, TimestampMixin
 class UserOrm(Base, TimestampMixin):
     __tablename__ = 'users'
 
-    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    tg_id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
     active: Mapped[bool] = mapped_column(server_default=true())
     language: Mapped[str] = mapped_column(default='ru')
+    banned: Mapped[bool] = mapped_column(server_default=false())
+
+    danbooru = relationship('DanbooruOrm', back_populates='user')
 
     def __repr__(self):
-        return (f'User(id={self.id}, active={self.active}, language={self.language},'
+        return (f'User(id={self.tg_id}, active={self.active}, language={self.language},'
                 f' created_at={self.created_at.strftime("%Y-%m-%d %H:%M:%S")},'
                 f' updated_at={self.updated_at.strftime("%Y-%m-%d %H:%M:%S")})')
 
     def to_dto(self) -> User:
         return User(
-            id=self.id,
+            tg_id=self.tg_id,
             active=self.active,
             language=self.language,
             created_at=self.created_at,
-            updated_at=self.updated_at
+            updated_at=self.updated_at,
+            banned=self.banned
         )
