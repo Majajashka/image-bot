cf6028dc84fddfefd1268ee919b42b2844c29724
majaja <majajashka@hotmail.com>
2024-06-18 03:32:51 +0400

    init(DanbooruResponseValidation): add post validation

---
+++ app/core/utils/validate_response.py
@@ -0,0 +1,11 @@
+from app.core.constants.danbooru import ALLOWED_FILE_TYPES
+from app.core.models.dto.api.danbooru import DanbooruPost
+from app.core.utils.expections import InvalidDanbooruPostData
+
+
+def validate_danbooru_post(post: DanbooruPost) -> DanbooruPost:
+    if post.file.ext not in ALLOWED_FILE_TYPES:
+        raise InvalidDanbooruPostData(f'Unallowed file ext. File ext: {post.file.ext}')
+    if not post.file.url:
+        raise InvalidDanbooruPostData(f'{type(post)} missing file url. ID: {post.id}')
+    return post
