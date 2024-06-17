2fb29c9a5e2785a8af7446cab0ad09349b9985b5
majaja <majajashka@hotmail.com>
2024-06-18 03:30:49 +0400

    feat(DanbooruPostTags): add html escape method

--- app/core/models/dto/api/danbooru.py
+++ app/core/models/dto/api/danbooru.py
@@ -1,7 +1,8 @@
+import html
 from dataclasses import dataclass
 from typing import Optional, Collection
 
-from app.core.utils.validate_args import validate_danbooru_tags
+from app.core.utils.validate_data import validate_danbooru_request_tags
 
 
 @dataclass
@@ -19,7 +20,7 @@ class DanbooruRequestArgs:
         if self.count <= 0:
             raise ValueError(f"Request count should be positive number, not {self.count}")
 
-        validate_danbooru_tags(self.tags)
+        validate_danbooru_request_tags(self.tags)
 
 
 @dataclass
@@ -30,6 +31,10 @@ class DanbooruPostTags:
     artist: str
     meta: str
 
+    @property
+    def general_tags_html_escape(self):
+        return html.escape(self.general)
+
 
 @dataclass
 class DanbooruPostFile:
@@ -37,6 +42,7 @@ class DanbooruPostFile:
     small_file_url: str
     preview_url: str
     file_size: int
+    ext: str
 
     def url_by_size(self, max_size: int = 10_485_760):
         if self.file_size > max_size:
@@ -66,7 +72,8 @@ class DanbooruPost:
                 url=data.get('file_url'),
                 small_file_url=data.get('large_file_url'),
                 preview_url=data.get('preview_url'),
-                file_size=data.get('file_size')
+                file_size=data.get('file_size'),
+                ext=data.get('file_ext')
             ),
             score=data.get('score'),
             rating=data.get('rating'),
