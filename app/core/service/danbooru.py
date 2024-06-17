7a67c1b4ba7b5485538b664e62a640017277542e
majaja <majajashka@hotmail.com>
2024-06-17 15:07:38 +0400

    feat(danbooru.service): add user danbooru settings get or create function

--- app/core/service/danbooru.py
+++ app/core/service/danbooru.py
@@ -1,11 +1,14 @@
 from typing import Collection, Optional
 
+from sqlalchemy.exc import NoResultFound
+
 from app.core.models.dto.api.danbooru import DanbooruPost, DanbooruRequestArgs
 from app.core.external_service.api.danbooru import DanbooruAPI
 from app.core.models.dto.api.parse_config import ParseConfig
+from app.core.models.dto.danbooru import UserDanbooruSettings
 from app.core.utils.expections import UserArgumentError
 from app.core.utils.parse_args import parse_args_for_post
-
+from app.infrastructure.database.repo.danbooru import DanbooruRepo
 
 
 
@@ -27,3 +30,11 @@ async def get_danbooru_post(tags: Optional[Collection[str]] = None) -> DanbooruP
     post = await danbooru.image(tags)
     await danbooru.session_close()
     return post
+
+
+async def get_or_create_user_danbooru(user_id: int, repo: DanbooruRepo) -> UserDanbooruSettings:
+    try:
+        user_danbooru = await repo.get_by_id(user_id)
+    except NoResultFound:
+        user_danbooru = await repo.create_for_user(user_id)
+    return user_danbooru
