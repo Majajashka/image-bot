ec8bb17e4bc9cb292f9fefceedbc58fb332cc7a0
majaja <majajashka@hotmail.com>
2024-06-17 14:59:42 +0400

    feat(UserRepo): get_by_id -> get_user_by_id and add upsert_user

--- app/infrastructure/database/repo/user.py
+++ app/infrastructure/database/repo/user.py
@@ -4,28 +4,32 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.models.dto.user import User
 from app.infrastructure.database.models.user import UserOrm
-from .base import BaseRepo
+from app.infrastructure.database.repo.base import BaseRepo
 
 
 class UserRepo(BaseRepo[UserOrm]):
     def __init__(self, session: AsyncSession):
         super().__init__(model=UserOrm, session=session)
 
-    async def get_by_id(self, user_id: int) -> User:
+    async def get_user_by_id(self, user_id: int) -> User:
         return (await self._get_by_id(user_id)).to_dto()
 
-    async def get_or_create_user(self, user: User) -> User:
-        insert_stmt = insert(UserOrm).values(id=user.id)
-        update_stmt = insert_stmt.on_conflict_do_update(
-            constraint='id',
-            set_=dict(
-                id=insert_stmt.excluded.id)
+    async def upsert_user(self, user: User) -> User:
+        stmt = (
+            insert(UserOrm)
+            .values(tg_id=user.tg_id)
+            .on_conflict_do_nothing()
+            .returning(UserOrm)
         )
 
-        saved_user = await self.session.execute(update_stmt)
+        saved_user = await self.session.execute(stmt)
         return saved_user.scalar_one().to_dto()
 
     async def get_users_count_by_status(self, status: bool = True) -> int:
-        users = await self.session.execute(select(func.count()).where(self.model.active == status))
+        stmt = (
+            select(func.count())
+            .where(self.model.active == status)
+        )
+        users = await self.session.execute(stmt)
         count = users.scalars().one()
         return count
