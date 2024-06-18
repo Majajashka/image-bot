55b219d929e8190d6497acfd79a498fe2b3a061d
majaja <majajashka@hotmail.com>
2024-06-18 04:42:23 +0400

    add missing code

---
+++ app/core/models/dto/danbooru.py
@@ -0,0 +1,24 @@
+from app.core.models.dto.base import Base
+
+
+class DanbooruDisplaySettings(Base):
+    tags: bool
+    score: bool
+    rating: bool
+    url: bool
+
+    def __repr__(self):
+        return f'DanbooruDisplaySettings({self.tags=}, {self.score=}, {self.rating=}, {self.url=}'
+
+
+class UserDanbooruSettings(Base):
+    default_tags: str
+    default_count: int
+    display: DanbooruDisplaySettings
+
+    def __repr__(self):
+        return f'UserDanbooruSettings({self.default_tags=}, {self.default_count=}, {repr(self.display)}'
+
+    @property
+    def default_tags_list(self):
+        return self.default_tags.split()
