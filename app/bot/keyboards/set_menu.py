a4b57ca0f49a225e3a287020a3f0190627e7cfdd
majaja <majajashka@hotmail.com>
2024-06-18 05:03:57 +0400

    feat(BotMenu): add /danbooru command

--- app/bot/keyboards/set_menu.py
+++ app/bot/keyboards/set_menu.py
@@ -4,8 +4,8 @@ from aiogram.types import BotCommand
 
 async def set_main_menu(bot: Bot) -> bool:
     main_menu_commands = [
-        BotCommand(command='/adminlist',
-                   description='Список админов')
+        BotCommand(command='/danbooru',
+                   description='<count> <tags>'),
     ]
 
     return await bot.set_my_commands(main_menu_commands)
