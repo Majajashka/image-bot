437acae1e22c23b41232e1f9ff95cd611c156c03
majaja <majajashka@hotmail.com>
2024-06-18 18:13:21 +0400

    feat(DanbooruAPI): Add tags search method

--- app/core/external_service/api/danbooru.py
+++ app/core/external_service/api/danbooru.py
@@ -1,8 +1,7 @@
 from typing import Optional, Collection
 from aiohttp import ClientResponse
 
-from app.core.external_service.api.base import BaseExternalAPI
-from app.core.models.dto.api.danbooru import DanbooruPost
+from app.core.external_service.api.base import BaseRequests
 from app.core.models.dto.api.danbooru import DanbooruPost, DanbooruTags
 from app.core.utils.expections import DanbooruApiError
 
@@ -26,3 +25,20 @@ class DanbooruAPI(BaseRequests):
         )
         return DanbooruPost.from_dict(data=data)
 
+    async def search_tags(
+            self,
+            tag: Optional[str],
+            regex: str = '.*{}.*',
+            order: str = 'count',
+            hide_empty: bool = True
+    ) -> list[DanbooruTags]:
+        params = {
+            'search[name_regex]': regex.format(tag or ''),
+            'search[hide_empty]': hide_empty,
+            'search[order]': order
+        }
+        data = await self._get(
+            endpoint='tags.json',
+            params=params
+        )
+        return [DanbooruTags.from_dict(tags) for tags in data]
