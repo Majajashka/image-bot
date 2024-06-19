cf355c161df3da77fe1cdb851fbaf15c52bcc307
majaja <majajashka@hotmail.com>
2024-06-19 04:47:59 +0400

    add str method for DanbooruDTO

--- app/core/models/dto/danbooru.py
+++ app/core/models/dto/danbooru.py
@@ -10,6 +10,8 @@ class DanbooruDisplaySettings(Base):
     def __repr__(self):
         return f'DanbooruDisplaySettings({self.tags=}, {self.score=}, {self.rating=}, {self.url=})'
 
+    def __str__(self):
+        return f'Display settings - Tags: {self.tags}, Score: {self.score}, Rating: {self.rating}, URL: {self.url}'
 
 class UserDanbooruSettings(Base):
     default_tags: str
@@ -18,6 +20,10 @@ class UserDanbooruSettings(Base):
 
     def __repr__(self):
         return f'UserDanbooruSettings({self.default_tags=}, {self.default_count=}, {repr(self.display)}'
+
+    def __str__(self):
+        return (f'User Settings - Default Tags: {self.default_tags}, Default Count: {self.default_count}, '
+                f'Display: {str(self.display)}')
 
     @property
     def default_tags_list(self):
