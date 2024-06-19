7efdb373704b2618c5fbfd311b2b82309d503e81
majaja <majajashka@hotmail.com>
2024-06-19 21:57:14 +0400

    fix(args parsing): fix args parsing for danbooru pot

--- app/bot/filters/admin.py
+++ app/bot/filters/admin.py
@@ -15,7 +15,6 @@ class IsChatAdmin(BaseFilter):
     async def __call__(self, message: Message, bot: Bot) -> bool:
         if message.chat.type == 'private':
             return False
-        member = await bot.get_chat_member(chat_id=message.chat.id, user_id=message.from_user.id)
-        if isinstance(member, (ChatMemberAdministrator, ChatMemberOwner)):
-            return True
-        return False
+        admins = await bot.get_chat_administrators(message.chat.id)
+        admins_id = {admin.user.id for admin in admins}
+        return message.from_user.id in admins_id
